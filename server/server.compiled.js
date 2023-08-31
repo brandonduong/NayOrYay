83b1ed134639b2ab698c61c537f3d8ff7ed8a713
Brandon Duong <brandon.duong@outlook.com>
2023-08-30 20:43:34 -0400

    deploy

--- server/server.compiled.js
+++ server/server.compiled.js
@@ -2,17 +2,23 @@
 
 var _path = _interopRequireDefault(require("path"));
 var _express = _interopRequireDefault(require("express"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = process.env.HTTP_PORT || 4001;
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+var PORT = 8081;
 var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, "../client", "build")));
+app.use(
+  _express["default"]["static"](
+    _path["default"].join(__dirname, "../client", "build")
+  )
+);
 app.get("/", function (req, res) {
   res.send("just gonna send it");
 });
 app.get("/flower", function (req, res) {
   res.json({
     name: "Dandelion",
-    colour: "Blue-ish"
+    colour: "Blue-ish",
   });
 });
 app.listen(PORT, function () {
